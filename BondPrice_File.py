c31bd8219d5a2251f0e697a3cbfcd0656ba697d8
ruixiangzhang1 <rz2791@columbia.edu>
2025-10-03 21:42:46 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -3,7 +3,7 @@
 def getBondPrice(y, face, couponRate, m, ppy=1):
     coupon_payment = (couponRate * face) / ppy
     total_periods = m * ppy
-    period_rate = y / ppy
+    period_rate = y 
     pv_coupons = sum([coupon_payment / (1 + period_rate)**t for t in range(1, total_periods + 1)])
     pv_face_value = face / (1 + period_rate)**total_periods
     bondPrice = pv_coupons + pv_face_value
