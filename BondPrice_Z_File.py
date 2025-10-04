f7311c78f00bcd7217ec00fffdcf590010466c70
ruixiangzhang1 <rz2791@columbia.edu>
2025-10-03 22:24:08 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,17 +1,17 @@
 
-
 def getBondPrice_Z(face, couponRate, times, yc):
-    couponPayment = face*couponRate
-    BondPrice=0
+    couponPayment = face * couponRate
+    BondPrice = 0.0
+
     for t, yc_rate in zip(times, yc):
-     if t == max(times):
+        if t == max(times):
             cash_flow = couponPayment + face
-     else:
+        else:
             cash_flow = couponPayment
-    pv_factor = 1 / (1 + yc_rate) ** t
-        
-    pvcf = cash_flow * pv_factor
-        
-    BondPrice += pvcf
-    
-    return(BondPrice)
+
+        pv_factor = 1 / (1 + yc_rate) ** t
+        pvcf = cash_flow * pv_factor
+
+        BondPrice += pvcf
+
+    return BondPrice
