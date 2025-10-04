d88848e08bb70f13556fa91a73e559cd11476fb4
ruixiangzhang1 <rz2791@columbia.edu>
2025-10-03 23:04:38 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,17 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    couponPayment = face * couponRate
+    bondPrice = 0.0
+    n = len(yc) 
+    
+    for i, r in enumerate(yc, start=1):
+        if i == n:
+            cash_flow = couponPayment + face
+        else:
+            cash_flow = couponPayment
+
+        pv_factor = 1 / (1 + r) ** i
+        bondPrice += cash_flow * pv_factor
+
+    return bondPrice
