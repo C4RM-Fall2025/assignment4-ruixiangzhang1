11d6122cc038f81ce2018b3653c73941054a62c0
ruixiangzhang1 <rz2791@columbia.edu>
2025-10-03 23:21:47 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -2,13 +2,13 @@
 
 def FizzBuzz(start, finish):
     outlist = []
-    for n in range(start, finish + 1):
-        s = ""
-        if n % 3 == 0:
-            s += "fizz"
-        if n % 5 == 0:
-            s += "buzz"
-        if not s:
-            s = str(n)
-        outlist.append(s)
+    for i in range(start, finish + 1):
+        if i % 15 == 0:
+            outlist.append("fizzbuzz")
+        elif i % 3 == 0:
+            outlist.append("fizz")
+        elif i % 5 == 0:
+            outlist.append("buzz")
+        else:
+            outlist.append(i)
     return outlist
