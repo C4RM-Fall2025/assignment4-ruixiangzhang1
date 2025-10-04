96103bbe25eb968790f4710be41d69d0220ccc72
ruixiangzhang1 <rz2791@columbia.edu>
2025-10-03 21:58:47 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,27 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+   coupon_payment = (couponRate * face) / ppy
+   
+    periods = m * ppy
+    period_rate = y / ppy
+    bond_price = 0  
+    weighted_sum = 0 
+    
+    for t in range(1, periods + 1):
+        
+        if t == periods:
+            cash_flow = coupon_payment + face
+        else:
+            cash_flow = coupon_payment
+        
+        pv_factor = 1 / (1 + period_rate) ** t
+        pvcf = cash_flow * pv_factor
+        
+        bond_price += pvcf
+        
+        weighted_sum += pvcf * t / ppy 
+   
+    bondDuration = weighted_sum / bond_price
+
+    return bondDuration
+
